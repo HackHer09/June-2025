590ee8613c547ce2b4dc375d666c61d407fc2bfd
Ananya Beckwoad <anu.ananya.beckwoad@gmail.com>
2025-06-27 19:01:09 +0530

    Add files via upload

---
+++ SLL.py
@@ -0,0 +1,20 @@
+class Node :
+    def __init__(self,val):
+        self.data = val
+        self.next=None
+        
+def createLinkedList(arr):
+    head = None 
+    for val in arr :
+        if(head==None):
+            head = Node(val)
+            temp = head
+        else :
+            temp.next =  Node(val)
+            temp = temp.next
+    return head.next.data
+arr = list(map(int,input().split()))
+print(createLinkedList(arr))
+
+
+
